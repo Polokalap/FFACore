48ccbaf5cb5df33a195eaf030dca4d8ea121c55b
polokalap <Polokalap@autizm.us>
2026-08-31 23:16:07 +0200

    Added command filters in combat.

--- src/main/java/mel/Polokalap/ffa/Listener/CombatListener.java
+++ src/main/java/mel/Polokalap/ffa/Listener/CombatListener.java
@@ -1,13 +1,19 @@
 package mel.Polokalap.ffa.Listener;
 
+import com.sk89q.worldedit.event.platform.CommandSuggestionEvent;
 import mel.Polokalap.ffa.Utils.ArenaUtil;
 import mel.Polokalap.ffa.Utils.CombatManager;
+import mel.Polokalap.ffa.Utils.ComponentUtil;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerCommandSendEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import static mel.Polokalap.ffa.Main.getInstance;
 
 public class CombatListener implements Listener {
 
@@ -38,6 +44,35 @@ public class CombatListener implements Listener {
         Player player = event.getPlayer();
 
         if (CombatManager.isInCombat(player)) player.setHealth(0.0d);
+
+    }
+
+    @EventHandler
+    public void onCommand(PlayerCommandPreprocessEvent event) {
+
+        Player player = event.getPlayer();
+
+        if (!CombatManager.isInCombat(player)) return;
+        if (player.hasPermission("ffa.admin")) return;
+
+        boolean cancel = true;
+
+        for (String entry : getInstance().getConfig().getStringList("settings.combat-commands")) {
+
+            if (event.getMessage().startsWith(entry)) cancel = false;
+
+        }
+
+        if (cancel) {
+
+            player.sendMessage(
+                    ComponentUtil.getComponent(
+                            "player.combat-command"
+                    )
+            );
+            event.setCancelled(true);
+
+        }
 
     }
 
